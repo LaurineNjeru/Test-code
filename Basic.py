df335ea231a001dbe4f98c87ff701943a2dec561
Laurine_Njeru <njerulaurine8@gmail.com>
2025-10-15 22:44:45 -0700

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,11 @@
+#Dmeonstrating data types in Python
+
+student_age=20 #integer
+average_score=75.8 #Float
+student_name="alice" #string
+is_enrolled=True #boolean
+
+print(type(student_age))
+print(type(average_score))
+print(type(student_name))
+print(type(is_enrolled))
